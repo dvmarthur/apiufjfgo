f742e5bb1b7806e7726950a220b44122be872686
Arthur Dias <arthurdvm2@gmail.com>
2023-06-28 20:25:32 -0300

    comentando rotas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('rides', RideController::class);
-Route::resource('users', UserController::class);
+//Route::resource('rides', RideController::class);
+//Route::resource('users', UserController::class);
 
 Route::get('/messages', [MessageController::class, 'index']);
 Route::post('/messages', [MessageController::class, 'store']);
