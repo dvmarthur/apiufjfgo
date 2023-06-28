93038a6974d823b2920470a65d9916a62950e7ba
dvmarthur <83983660+dvmarthur@users.noreply.github.com>
2023-06-28 11:36:07 +0000

    migrations

---
+++ database/migrations/2023_06_28_113053_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('matricula')->nullable(); // Adiciona campo 'matricula' que pode ser nulo
+            $table->string('numero_carteira_motorista')->nullable(); // Adiciona campo 'numero_carteira_motorista' que pode ser nulo
+            $table->string('password');
+            $table->unsignedBigInteger('user_type_id'); // Chave estrangeira
+            $table->timestamps();
+
+            // Relacionamento com a tabela 'user_types'
+            $table->foreign('user_type_id')->references('id')->on('user_types');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
